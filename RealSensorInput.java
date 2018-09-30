0895345e31477d02de3c748346b9ea0b1c750187
David Reeves <djreeves2005@gmail.com>
2018-09-30 14:20:30 -0400

    Code Transfer (IG)
    Ignore

---
+++ RealSensorInput.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Sensor Read Thing", group = "")
+public class template extends LinearOpMode {
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public static void main(String[ ] args) {
+        String hex1 = "FFFF00";
+        String hex2 = "FFFFFF";
+        String hex31 = "FFFFFF";
+
+        int decimal1 = Integer.parseInt(hex1,16);
+        int decimal2 = Integer.parseInt(hex2,16);
+        int decimal3 = Integer.parseInt(hex31,16);
+
+        int dif1 = decimal1-decimal3;
+        int dif2 = decimal2-decimal3;
+
+        telemetry.addline(dif1);
+        telemetry.addline(dif2);
+        telemetry.update();
+
+        if (Math.abs(dif1) > Math.abs(dif2)) {
+            telemetry.addline("White, moving on.");
+            String hex32 = "FFFFFF";
+
+            int decimal4 = Integer.parseInt(hex1,16);
+            int decimal5 = Integer.parseInt(hex2,16);
+            int decimal6 = Integer.parseInt(hex32,16);
+
+            int dif3 = decimal4-decimal6;
+            int dif4 = decimal5-decimal6;
+
+            telemetry.addline(dif3);
+            telemetry.addline(dif4);
+            telemetry.update();
+
+            if (Math.abs(dif3) > Math.abs(dif4)) {
+                telemetry.addline("White, moving on.");
+                String hex33 = "FFFF00";
+
+                int decimal7 = Integer.parseInt(hex1,16);
+                int decimal8 = Integer.parseInt(hex2,16);
+                int decimal9 = Integer.parseInt(hex33,16);
+
+                int dif5 = decimal7-decimal9;
+                int dif6 = decimal8-decimal9;
+
+                telemetry.addline(dif5);
+                telemetry.addline(dif6);
+                if (Math.abs(dif5) > Math.abs(dif6)) {
+                    telemetry.addline("White, ERROR");
+
+                }else if (Math.abs(dif5) < Math.abs(dif6)) {
+                    telemetry.addline("Gold, found in position 3");
+                }
+            }
+            else if (Math.abs(dif3) < Math.abs(dif4)) {
+                telemetry.addline("Gold, found target in position 2");
+            }
+        }
+        else if (Math.abs(dif1) < Math.abs(dif2)) {
+            telemetry.addline("Gold, found target in position 1");
+        }
+    }
+}
