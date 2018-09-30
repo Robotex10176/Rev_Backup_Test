529bdc39762781c1103acc4a05387d25baa2af6b
Fritz <github-f@standland.org>
2018-09-30 17:06:47 -0400

    finished

--- JewelExample.java
+++ JewelExample.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 @Autonomous(name = "JewelExample", group = "")
-public class SensorOutput_Copy extends LinearOpMode {
+public class JewelExample extends LinearOpMode {
 
   private ColorSensor blockSensor;
 
@@ -19,17 +19,35 @@ public class SensorOutput_Copy extends LinearOpMode {
     if (opModeIsActive()) {
       int blue1;
       int blue2;
-      int blue3
-      int blue
+      int blue3;
+      int blue;
       //assuming you start at first jewel
       blue1 = blockSensor.blue();
-      sleep(100)
+      sleep(100);
       blue2 = blockSensor.blue();
-      sleep(100)
+      sleep(100);
       blue3 = blockSensor.blue();
-      sleep(100)
-      blue = (blue1 + blue2 + blue3)/3
-      telemetry.addLine(integer.tostring(blue));
+      sleep(100);
+      blue = ((blue1 + blue2 + blue3)/3);
+      if (blue > 75){
+        //Jewel is not gold, move on
+        //at this point robot is at second jewel
+        blue1 = blockSensor.blue();
+        sleep(100);
+        blue2 = blockSensor.blue();
+        sleep(100);
+        blue3 = blockSensor.blue();
+        sleep(100);
+        blue = ((blue1 + blue2 + blue3)/3);
+        if (blue > 75){
+          //jewel is not gold, move to third and push it
+        }else{
+          //jewel is gold, move it
+        }
+      }else{
+        //Jewel is gold, move it.
+      }
+        
       
       while (opModeIsActive()) {
         
